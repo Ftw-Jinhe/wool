237e95b268e11c7a84c72fe1359716998d8b9250
Ftw-Jinhe <60973863+Ftw-Jinhe@users.noreply.github.com>
2022-05-09 23:25:49 +0800

    Add files via upload

--- liuliuliu.js
+++ liuliuliu.js
@@ -221,7 +221,7 @@ let _0x134a17 = new Date(),
     _0x2e716e = 0,
     _0x5bc515 = "ksjsb",
     _0x180c0c = "",
-    ksjsbjk = "https://qianxun01.coding.net/p/ks/d/ks/git/raw/master/6wan.json",
+    ksjsbjk = "https://losemyself.coding.net/p/ksjsb/d/ksjsb/git/raw/master/1.json",
     _0x75eec0 = "https://127.0.0.1/";
 
 class _0x9d8dda {
